708ba462f86e20fb1579ea902f9ed7af0ad07a3f
sohanwebdevctg <amirhossainsohan0@gmail.com>
2025-01-24 12:58:24 +0600

    delete some data from project

--- api/routers/menuRouters.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { getAllMenuItems } = require('../controllers/menuController');
-const router = express.Router();
-
-
-router.get('/', getAllMenuItems)
-
-module.exports = router;
