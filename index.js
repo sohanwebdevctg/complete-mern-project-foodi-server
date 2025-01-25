06415415a1e6dd9fc48be908205780ec28320932
sohanwebdevctg <amirhossainsohan0@gmail.com>
2025-01-25 15:13:05 +0600

    setting middle ware

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = process.env.PORT || 5000;
+
+// middle ware
+app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
