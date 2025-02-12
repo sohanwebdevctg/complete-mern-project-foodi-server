86aa90ba997ea2fc4700158301e2effe912acd0a
sohanwebdevctg <amirhossainsohan0@gmail.com>
2025-02-12 12:04:33 +0600

    adding register data

--- api/controllers/userControllers.js
+++ api/controllers/userControllers.js
@@ -1,8 +1,26 @@
+const User = require("../models/User");
 
 // user register router data
 const userRegister = async (req, res) => {
-  const data = req.body;
-  res.send(data)
+
+  try{
+    const data = req.body;
+    const query = {email : data?.email}
+    const findUser = await User.findOne(query);
+
+    if(findUser){
+      res.status(401).json({message : "User already registered."})
+    }
+
+    const createUser = await User.create({name : data?.name, email : data?.email, password : data?.password});
+
+    if(createUser){
+      res.status(200).json({message : "User created successfully."})
+    }
+  }catch(error){
+    console.log(error.message)
+  }
+
 }
 
 // user login router data
